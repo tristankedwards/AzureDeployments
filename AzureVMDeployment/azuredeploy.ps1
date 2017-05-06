bc3c20baaf42cf13a9e4a17e26f8970bc6d2fede
tristankedwards <tristan.k.edwards@gmail.com>
2017-05-06 22:59:57 +0100

    Ready

---
+++ AzureVMDeployment/azuredeploy.ps1
@@ -0,0 +1,22 @@
+﻿#import-module azurerm
+
+#login-azurermaccount
+
+New-AzureRMResourceGroup -Name Bacon1234 -Location "West US" 
+
+$Deployment = @{
+	Name = 'BaconDeployment1';
+	ResourceGroupname = 'Bacon1234';
+	Mode = 'Complete';
+	TemplateFile = "$PSScriptRoot\azuredeploy.json";
+	TemplateParameterObject = @{
+		newStorageAccountName = 'Bacon1234';
+		vmName = 'tkebacon';
+		adminUsername = 'edwardst';
+		adminPassword = 'SuperCoolPassword123!';
+		dnsNameForPublicIP = 'tkebaconvmtest';
+		
+	}
+	Force = $true;
+}
+New-AzureRMResourceGroupDeployment @Deployment;
